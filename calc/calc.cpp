833be9c938620e0b9836b6337e6a1e5945afdc42
El-king16 <114861606+El-king16@users.noreply.github.com>
2024-09-14 21:50:11 +0300

    Add files via upload

---
+++ calc/calc.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    char choise;
+    
+    cout << "Напишите первое число = ";
+    cin >> a;
+    cout << "Напишите первое число = ";
+    cin >> b;
+    
+    cout << "Выберите +, -, *, /: ";
+    cin >> choise;
+    switch (choise)
+    {
+    case '+':
+        cout << a + b;
+    case '-':
+        cout << a - b;
+    case '*':
+        cout << a + b;
+    case '/':
+        
+        cout << a / b;
+    default:
+        break;
+    }
+}
+
